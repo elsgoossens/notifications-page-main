8f41e198969c7a997f0abac9abf12935fdfc5836
elsgoossens <elsgoo@gmail.com>
2023-04-13 15:48:26 +0200

    solution

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var btnRead = document.getElementById("markRead");
 var statusMarkers = document.getElementsByClassName("status");
+var newNotifications = document.getElementsByClassName("new-notification");
 
 btnRead.addEventListener("click", function(ev){
+    try{
     for (const s of statusMarkers) {
         if(s.classList.contains("unread")) s.classList.toggle("unread");
     }
+    for (const n of newNotifications) {
+        n.classList.remove("new-notification");
+    }
+}
+catch(ex){
+    console.log("error toggling new status off: " + ex);
+}
 })

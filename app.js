f567d1e7b8dfbdef924a1e1e6d6e6ea2b16f5d07
elsgoossens <elsgoo@gmail.com>
2023-04-14 13:39:04 +0200

    fixed issue with loop delay

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var newNotifications = document.getElementsByClassName("new-notification");
 
 btnRead.addEventListener("click", function(ev){
     try{
+        let time_out = 100;
     for (const s of statusMarkers) {
         if(s.classList.contains("unread")) s.classList.toggle("unread");
     }
     for (const n of newNotifications) {
-        n.classList.remove("new-notification");
+        setTimeout(() => {
+            n.classList.remove("new-notification"); 
+        }, time_out);
+        time_out+=100;
     }
     counter.innerHTML="0";
 }
